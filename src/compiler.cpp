8b8882b72c1841675beb47e312b93068e17dc53c
Chaz <8549883+stochaztic@users.noreply.github.com>
2021-03-06 17:43:06 -0500

    Experimental filesystem bugfix hack

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -10,6 +10,9 @@
 #include <sstream>
 
 #ifdef _WIN32
+	#ifndef _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+	#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+	#endif // !_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 	#include <experimental/filesystem>
 	namespace fs = std::experimental::filesystem::v1;
 #else
